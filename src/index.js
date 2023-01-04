38c1a7b0a059409e61a4c665b881cd59e3404d20
levideutsch <levideutsch@live.com>
2023-01-04 13:06:24 -0500

    commit tree 01df1332d453f70fa4cf9d65221a6d10f8f652f6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,111 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
-function App() {
-  return <Home />;
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+// /* Add basic styling for NavLinks */
+// const linkStyles = {
+//   display: "inline-block",
+//   width: "50px",
+//   padding: "12px",
+//   margin: "0 6px 6px",
+//   background: "blue",
+//   textDecoration: "none",
+//   color: "white",
+// };
+
+// function NavBar() {
+//   return (
+//     <div>
+//       <NavLink
+//       to="/"
+//       exact
+//       style={linkStyles}
+//       activeStyle={{
+//         background: "darkblue"
+//       }}
+//       >
+//         Home
+//         </NavLink>
+//         <NavLink 
+//         to="/about"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue"
+//         }}
+//         >
+//           About
+//         </NavLink>
+//         <NavLink
+//         to="/login"
+//         exact
+//         style={linkStyles}
+//         activeStyle={{
+//           background: "darkblue"
+//         }}
+//         >
+//           Login
+//         </NavLink>
+//     </div>
+//   )
+// }
+
+
+// function Home() {
+//   return <h1>Home!</h1> 
+// }
+
+// function About() {
+//   return <h1>This is my about component!</h1>
+// }
+
+// function Login() {
+//   return (
+//     <div> 
+//       <h1>Login</h1>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username"/>
+//           </div>
+//           <div>
+//             <input type="password" name="password" placeholder="Password"/>
+//           </div>
+//           <input type="submit" value="submit"/>
+//       </form>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <Switch>
+//         <Route path="/about">
+//           <About />
+//         </Route>
+//         <Route path="/login">
+//           <Login />
+//         </Route>
+//         <Route path="/">
+//           <Home />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
